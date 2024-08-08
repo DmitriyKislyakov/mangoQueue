caaaadda2618b9e16374e0f279e5e5c18a7797e1
Dima <125818471+DmitriyKislyakov@users.noreply.github.com>
2024-08-08 20:06:54 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import collections
+from collections import deque
 class Person:
     def __init__(self, name, profession):
         self.name = name
         self.profession = profession
 
 
+#persons
 you = Person('Dima', 'proger')
 juli = Person('Juli', 'engeneer')
 pasha = Person('Pasha', profession='taxi')
@@ -16,13 +17,33 @@ nastya = Person('Nastya', 'businessman')
 katya = Person('Katya', 'bank-manager')
 alena = Person('Alena', 'visage')
 
+#persons friends
 myFriends = [juli, pasha, kirill]
 pashaFriends = [kirill, andrey, katyaP]
 kirillFriends = [pasha, misha, nastya]
 juliFriends = [katya, alena]
-visited = set()
-graph = {you: myFriends, juli: juliFriends, pasha: pashaFriends, kirill: kirillFriends}
+
+#friends graphs
+graph = {you: myFriends, juli: juliFriends, pasha: pashaFriends, kirill: kirillFriends, andrey: [], katyaP: [], misha: [], nastya: [], katya: [], alena:[alena]}
+
+#search seller function:
+def searchSeller():
+    searched = set()
+    sellerDeque = deque()
+    sellerDeque += graph[you]
+    while sellerDeque:
+        personDeque = sellerDeque.popleft()
+        print(personDeque.name, personDeque.profession)
+        if not personDeque in searched:
+            if personDeque.profession == 'seller':
+                return print(f'{personDeque.name} is seller!')
+            else:
+                sellerDeque += graph[personDeque]
+            searched.add(personDeque)
+    return print('Nobody is seller')
+
+
+
 
 if __name__ == '__main__':
-    print(you.__dict__)
-    print(graph[juli])
+    searchSeller()
